cc350244ff26af9f78e5cb9e647906c69eaf568f
reeson <reeson2003@gmail.com>
2019-08-04 21:32:10 +0300

    task 2

--- lib/super.ex
+++ lib/super.ex
@@ -1,56 +1,73 @@
 defmodule Super do
-  @moduledoc """
-  Documentation for Super.
-  """
+  #  def ekaterinburg(list) do
+  #    Enum.filter(list, fn (x) -> !String.starts_with?(x, "hram") end)
+  #  end
+  #
+  #  def fizzbuzz(x) do
+  #    n = if rem(x, 5) == 0 && rem(x, 3) == 0 do
+  #      "FizzBuzz"
+  #    else
+  #      if rem(x, 3) == 0 do
+  #        "Fizz"
+  #      else
+  #        if rem(x, 5) == 0 do
+  #          "Buzz"
+  #        else
+  #          x
+  #        end
+  #      end
+  #    end
+  #    IO.inspect(n)
+  #  end
+  #
+  #  def fizzbuzz_2(x) do
+  #    n = case {rem(x, 5), rem(x, 3)} do
+  #      {0, 0} -> "FizzBuzz"
+  #      {_, 0} -> "Fizz"
+  #      {0, _} -> "Buzz"
+  #      {_, _} -> x
+  #    end
+  #    IO.inspect(n)
+  #  end
+  #
+  #  def fizzbuzz_3(x) do
+  #    n = cond do
+  #      rem(x, 5) == 0 && rem(x, 3) == 0 -> "FizzBuzz"
+  #      rem(x, 5) == 0 -> "Buzz"
+  #      rem(x, 3) == 0 -> "Fizz"
+  #      true -> x
+  #    end
+  #    IO.inspect(n)
+  #  end
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> Super.hello()
-      :world
-
-  """
-  def ekaterinburg(list) do
-    Enum.filter(list, fn (x) -> !String.starts_with?(x, "hram") end)
-  end
-
-  def fizzbuzz(x) do
-    n = if rem(x, 5) == 0 && rem(x, 3) == 0 do
-      "FizzBuzz"
-    else
-      if rem(x, 3) == 0 do
-        "Fizz"
-      else
-        if rem(x, 5) == 0 do
-          "Buzz"
-        else
-          x
-        end
+  #{:alica, "female"}
+  def girls_to_a_boys_to_b(list) do
+    Super.sort(
+      list,
+      %{"class_a" => [], "class_b" => []},
+      fn element -> case element do
+                      {_, "female"} -> "class_a"
+                      {_, "male"} -> "class_b"
+                    end
       end
-    end
-    IO.inspect(n)
+    )
   end
 
-  def fizzbuzz_2(x) do
-    n = case {rem(x, 5), rem(x, 3)} do
-      {0, 0} -> "FizzBuzz"
-      {_, 0} -> "Fizz"
-      {0, _} -> "Buzz"
-      {_, _} -> x
+  def sort(list, map, predicate) do
+    if Enum.empty?(list) do
+      map
+    else
+      Super.sort(
+        List.delete(list, List.first(list)),
+        Super.put(List.first(list), predicate.(List.first(list)), map),
+        predicate
+      )
     end
-    IO.inspect(n)
   end
 
-  def fizzbuzz_3(x) do
-    n = cond do
-      rem(x, 5) == 0 && rem(x, 3) == 0 -> "FizzBuzz"
-      rem(x, 5) == 0 -> "Buzz"
-      rem(x, 3) == 0 -> "Fizz"
-      true -> x
-    end
-    IO.inspect(n)
+  def put(element, key, map) do
+    Map.put(map, key, List.wrap(Map.get(map, key)) ++ [element])
   end
 
 end
